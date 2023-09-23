99da073d6f71d602d4ecfc2f3328d4d2b0ef37c7
Riann <ooriannsilva@gmail.com>
2023-09-23 14:11:33 -0300

    Templates Added and Testing

--- form/controller/default.py
+++ form/controller/default.py
@@ -1,6 +1,16 @@
 from form import app
+from flask import render_template, redirect, url_for, request, abort
 
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def register():
-    return "Hello, World!"
+    if request.method == 'POST':
+        if request.form['name'] == 'test' and request.form['email'] =='test@gmail.com':
+            return redirect(url_for('success'))    
+    return render_template('auth/index.html')
+    
+
+
+@app.route('/success')
+def success():
+    return render_template('results/success.html')
